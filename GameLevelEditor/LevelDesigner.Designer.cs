99e4e6a766ec3b9aa12df5ac717f68c5a1a3a9b3
Van Damage <jeannoelseneque@hotmail.com>
2018-11-20 12:57:45 +1030

    Made loads of changes
    grid on spritesheet and canvas
    able to show red highlight on selected tile on spritesheet
    able to select a tile on canvas

--- GameLevelEditor/LevelDesigner.Designer.cs
+++ GameLevelEditor/LevelDesigner.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.CanvasPBox = new System.Windows.Forms.PictureBox();
-            this.spritesheetPBox = new System.Windows.Forms.PictureBox();
             this.buttonLoad = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,31 +38,25 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxSpacing = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.spritesheetPBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.CanvasPBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.spritesheetPBox)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // CanvasPBox
             // 
             this.CanvasPBox.Location = new System.Drawing.Point(13, 29);
             this.CanvasPBox.Name = "CanvasPBox";
-            this.CanvasPBox.Size = new System.Drawing.Size(808, 409);
+            this.CanvasPBox.Size = new System.Drawing.Size(811, 638);
             this.CanvasPBox.TabIndex = 0;
             this.CanvasPBox.TabStop = false;
             this.CanvasPBox.Click += new System.EventHandler(this.CanvasPBox_Click);
             // 
-            // spritesheetPBox
-            // 
-            this.spritesheetPBox.Location = new System.Drawing.Point(828, 29);
-            this.spritesheetPBox.Name = "spritesheetPBox";
-            this.spritesheetPBox.Size = new System.Drawing.Size(360, 380);
-            this.spritesheetPBox.TabIndex = 1;
-            this.spritesheetPBox.TabStop = false;
-            this.spritesheetPBox.Click += new System.EventHandler(this.spritesheetPBox_Click);
-            // 
             // buttonLoad
             // 
-            this.buttonLoad.Location = new System.Drawing.Point(1113, 415);
+            this.buttonLoad.Location = new System.Drawing.Point(1114, 644);
             this.buttonLoad.Name = "buttonLoad";
             this.buttonLoad.Size = new System.Drawing.Size(75, 23);
             this.buttonLoad.TabIndex = 2;
@@ -76,7 +69,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.Size = new System.Drawing.Size(116, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "Game Level Canvas";
             // 
@@ -85,22 +78,22 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(828, 10);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.Size = new System.Drawing.Size(69, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "Spritesheet";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(829, 418);
+            this.label3.Location = new System.Drawing.Point(830, 647);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.Size = new System.Drawing.Size(41, 15);
             this.label3.TabIndex = 5;
             this.label3.Text = "Width:";
             // 
             // textBoxWidth
             // 
-            this.textBoxWidth.Location = new System.Drawing.Point(873, 415);
+            this.textBoxWidth.Location = new System.Drawing.Point(874, 644);
             this.textBoxWidth.Name = "textBoxWidth";
             this.textBoxWidth.Size = new System.Drawing.Size(33, 20);
             this.textBoxWidth.TabIndex = 6;
@@ -108,7 +101,7 @@
             // 
             // textBoxHeight
             // 
-            this.textBoxHeight.Location = new System.Drawing.Point(973, 415);
+            this.textBoxHeight.Location = new System.Drawing.Point(974, 644);
             this.textBoxHeight.Name = "textBoxHeight";
             this.textBoxHeight.Size = new System.Drawing.Size(33, 20);
             this.textBoxHeight.TabIndex = 8;
@@ -117,15 +110,15 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(927, 418);
+            this.label4.Location = new System.Drawing.Point(928, 647);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.Size = new System.Drawing.Size(46, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Height:";
             // 
             // textBoxSpacing
             // 
-            this.textBoxSpacing.Location = new System.Drawing.Point(1074, 415);
+            this.textBoxSpacing.Location = new System.Drawing.Point(1075, 644);
             this.textBoxSpacing.Name = "textBoxSpacing";
             this.textBoxSpacing.Size = new System.Drawing.Size(33, 20);
             this.textBoxSpacing.TabIndex = 10;
@@ -134,17 +127,35 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1023, 418);
+            this.label5.Location = new System.Drawing.Point(1024, 647);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.Size = new System.Drawing.Size(55, 15);
             this.label5.TabIndex = 9;
             this.label5.Text = "Spacing:";
+            // 
+            // spritesheetPBox
+            // 
+            this.spritesheetPBox.Location = new System.Drawing.Point(3, 3);
+            this.spritesheetPBox.Name = "spritesheetPBox";
+            this.spritesheetPBox.Size = new System.Drawing.Size(431, 603);
+            this.spritesheetPBox.TabIndex = 2;
+            this.spritesheetPBox.TabStop = false;
+            this.spritesheetPBox.Click += new System.EventHandler(this.spritesheetPBox_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.spritesheetPBox);
+            this.panel1.Location = new System.Drawing.Point(833, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 609);
+            this.panel1.TabIndex = 11;
             // 
             // LevelDesigner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 450);
+            this.ClientSize = new System.Drawing.Size(1282, 679);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBoxSpacing);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxHeight);
@@ -154,12 +165,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonLoad);
-            this.Controls.Add(this.spritesheetPBox);
             this.Controls.Add(this.CanvasPBox);
             this.Name = "LevelDesigner";
             this.Text = "Level Designer";
+            this.Shown += new System.EventHandler(this.LevelDesigner_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.CanvasPBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.spritesheetPBox)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,7 +180,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox CanvasPBox;
-        private System.Windows.Forms.PictureBox spritesheetPBox;
         private System.Windows.Forms.Button buttonLoad;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -178,6 +189,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxSpacing;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox spritesheetPBox;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- GameLevelEditor/LevelDesigner.cs
+++ GameLevelEditor/LevelDesigner.cs
@@ -14,6 +14,11 @@ namespace GameLevelEditor
         public Spritesheet Spritesheet { get; private set; }
         Bitmap canvasArea, spritesheetArea;
 
+        int gridWidth = 16;
+        int gridHeight = 16;
+        int spacing = 0;
+
+
         public Point CurrentTile { get; private set; } = new Point();
 
         public string Filename
@@ -31,13 +36,41 @@ namespace GameLevelEditor
 
             // get the dimensions of the canvas area
             canvasArea = new Bitmap(CanvasPBox.Width, CanvasPBox.Height);
-            spritesheetArea = new Bitmap(360, 380); // fix these later
+            spritesheetArea = new Bitmap (spritesheetPBox.Width, spritesheetPBox.Height); // fix these later
             clearCanvas();
             clearSS();
         }
 
         private void CanvasPBox_Click(object sender, EventArgs e)
         {
+            if (Spritesheet == null)
+                return;
+
+            // get tile coordinate of click 
+            if (e.GetType() == typeof(MouseEventArgs))
+            {
+                MouseEventArgs mouse = e as MouseEventArgs;
+
+                // draw the current tile there
+                CurrentTile = new Point(
+                    mouse.X / (gridWidth + spacing)
+                    , mouse.Y / (gridHeight + spacing));
+
+                drawGridOnCanvas();
+            }
+        }
+
+        private void DrawTile()
+        {
+            // assign the drawing canvas
+            Graphics g = Graphics.FromImage(canvasArea);
+
+            // define the size of the tile
+            // understand what << 2 means exactly
+            Rectangle dest = new Rectangle(0, 0, gridWidth << 2, gridHeight << 2);
+
+
+
 
         }
 
@@ -48,17 +81,33 @@ namespace GameLevelEditor
 
         private void textBoxWidth_TextChanged(object sender, EventArgs e)
         {
+            if (int.TryParse(textBoxWidth.Text, out gridWidth) == true)
+            {
+                drawGrid();
+            }
 
+            textBoxWidth.Text = gridWidth.ToString();
         }
 
         private void textBoxHeight_TextChanged(object sender, EventArgs e)
         {
+            if (int.TryParse(textBoxHeight.Text, out gridHeight) == true)
+            {
+                drawGrid();
+            }
+
+            textBoxHeight.Text = gridHeight.ToString();
 
         }
 
         private void textBoxSpacing_TextChanged(object sender, EventArgs e)
         {
+            if (int.TryParse(textBoxSpacing.Text, out spacing) == true)
+            {
+                drawGrid();
+            }
 
+            textBoxSpacing.Text = spacing.ToString();
         }
 
         private void buttonLoad_Click(object sender, EventArgs e)
@@ -70,12 +119,19 @@ namespace GameLevelEditor
                 if (dlg.CheckFileExists == true)
                 {
                     Spritesheet = new Spritesheet(dlg.FileName);
+                    spritesheetArea = new Bitmap(Spritesheet.Width, Spritesheet.Height);
                     drawGrid();
                 }
             }
         }
 
         private void drawGrid()
+        {
+            drawGridOnCanvas();
+            drawGridOnSpritesheet();
+        }
+
+        private void drawGridOnCanvas()
         {
             CanvasPBox.DrawToBitmap(canvasArea, CanvasPBox.Bounds);
 
@@ -88,33 +144,77 @@ namespace GameLevelEditor
                 return;
 
             // draw the spritesheet (regardless of size mine you) to the
-            g.DrawImage(Spritesheet.Image, 0, 0);
+            //g.DrawImage(Spritesheet.Image, 0, 0);
+            //g.DrawImageU
 
-            Pen pen = new Pen(Brushes.Black);
+            Pen pen = new Pen(Brushes.LightGray);
 
             int height = CanvasPBox.Height;
             int width = CanvasPBox.Width;
-            for (int y = 0; y < height; y += Spritesheet.GridHeight + Spritesheet.Spacing)
+            for (int y = 0; y < height; y += gridHeight + spacing)
             {
                 g.DrawLine(pen, 0, y, width, y);
             }
 
-            for (int x = 0; x < width; x += Spritesheet.GridWidth + Spritesheet.Spacing)
+            for (int x = 0; x < width; x += gridWidth + spacing)
             {
                 g.DrawLine(pen, x, 0, x, height);
             }
 
             Pen highlight = new Pen(Brushes.Red);
             g.DrawRectangle(highlight,
-                CurrentTile.X * (Spritesheet.GridWidth + Spritesheet.Spacing),
-                CurrentTile.Y * (Spritesheet.GridHeight + Spritesheet.Spacing),
-                Spritesheet.GridWidth + Spritesheet.Spacing,
-                Spritesheet.GridHeight + Spritesheet.Spacing);
+                CurrentTile.X * (gridWidth + spacing),
+                CurrentTile.Y * (gridHeight + spacing),
+                gridWidth + spacing,
+                gridHeight + spacing);
 
 
             g.Dispose();
 
             CanvasPBox.Image = canvasArea;
+        }
+
+        private void drawGridOnSpritesheet()
+        {
+            spritesheetPBox.DrawToBitmap(spritesheetArea, spritesheetPBox.Bounds);
+
+            Graphics g;
+            g = Graphics.FromImage(spritesheetArea);
+
+            g.Clear(Color.White);
+
+            if (Spritesheet == null)
+                return;
+
+            // draw the spritesheet (regardless of size mine you) to the
+            g.DrawImage(Spritesheet.Image, 0, 0);
+            //g.DrawImageU
+
+            Pen pen = new Pen(Brushes.LightGray);
+
+            int height = spritesheetPBox.Height;
+            int width = spritesheetPBox.Width;
+            for (int y = 0; y < height; y += gridHeight + spacing)
+            {
+                g.DrawLine(pen, 0, y, width, y);
+            }
+
+            for (int x = 0; x < width; x += gridWidth + spacing)
+            {
+                g.DrawLine(pen, x, 0, x, height);
+            }
+
+            Pen highlight = new Pen(Brushes.Red);
+            g.DrawRectangle(highlight,
+                CurrentTile.X * (gridWidth + spacing),
+                CurrentTile.Y * (gridHeight + spacing),
+                gridWidth + spacing,
+                gridHeight + spacing);
+
+
+            g.Dispose();
+
+            spritesheetPBox.Image = spritesheetArea;
         }
 
         // clear canvas 
@@ -129,6 +229,28 @@ namespace GameLevelEditor
             g.Dispose();
 
             CanvasPBox.Image = canvasArea;
+        }
+
+        private void spritesheetPBox_Click_1(object sender, EventArgs e)
+        {
+            if (Spritesheet == null)
+                return;
+            
+            // get tile coordinate of click 
+            if (e.GetType() == typeof(MouseEventArgs))
+            {
+                MouseEventArgs mouse = e as MouseEventArgs;
+                CurrentTile = new Point( 
+                    mouse.X / (gridWidth + spacing)
+                    , mouse.Y / (gridHeight + spacing));
+
+                drawGridOnSpritesheet();
+            }
+        }
+
+        private void LevelDesigner_Shown(object sender, EventArgs e)
+        {
+            drawGrid();
         }
 
         // clear spritesheet area
